bbd27f068822e37dd70991fb00662d251a85c582
Joaquim Torres <quimrstorres@gmail.com>
2014-05-30 00:55:49 +0100

    Add script to install tools through homebrew

---
+++ bootapps.sh
@@ -0,0 +1,20 @@
+brew install tmux
+brew install rbenv
+brew install ruby-build
+brew install mysql
+brew install ag
+brew install sbt
+brew install leiningen
+
+brew tap homebrew/dupes
+brew install apple-gcc42
+
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install adium
+brew cask install hipchat
+brew cask install sequel-pro
+brew cask install iterm2
+
+brew cask install atom
+apm install vim-mode
