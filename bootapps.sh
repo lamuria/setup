b8473788890732d598660050b3e8ea94d79562c4
Joaquim Torres <quimrstorres@gmail.com>
2014-05-30 01:18:28 +0100

    Add some apps

--- bootapps.sh
+++ bootapps.sh
@@ -11,10 +11,15 @@ brew install apple-gcc42
 
 brew install caskroom/cask/brew-cask
 brew cask install google-chrome
+brew cask install firefox
 brew cask install adium
 brew cask install hipchat
 brew cask install sequel-pro
 brew cask install iterm2
+brew cask install alfred
+brew cask install vlc
+brew cask install utorrent
+brew cask install tvshows
 
 brew cask install atom
 apm install vim-mode
