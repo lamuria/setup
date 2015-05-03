d8a796346aaefd7fd5b6b207cdf4e12d1517b930
Joaquim Torres <quim.r.s.torres@gmail.com>
2015-05-03 20:51:44 +0100

    Update items to install

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,11 +1,14 @@
 brew install zsh
+brew install zsh-completions
+brew install zsh-syntax-highlighting
+brew install zsh-history-substring-search
 brew install tmux
-brew install git bash-completion
+brew install reattach-to-user-namespace
+brew install git
 brew install ctags
 brew install rbenv
 brew install ruby-build
 brew install ag
-brew install sbt
 brew install leiningen
 brew install hub
 brew install node
@@ -21,13 +24,14 @@ brew install caskroom/cask/brew-cask
 brew cask install java
 brew cask install google-chrome
 brew cask install firefox
-brew cask install onepassword
+brew cask install 1password
 brew cask install google-drive
 brew cask install adium
 brew cask install hipchat
+brew cask install slack
 brew cask install iterm2
-brew cask install paw
-brew cask install alfred
+# brew cask install alfred
+# brew cask install flashlight
 brew cask install spotify
 brew cask install vlc
 brew cask install utorrent
@@ -36,6 +40,3 @@ brew cask install evernote
 brew cask install skype
 brew cask install limechat
 brew cask install gitter
-
-brew cask install atom
-apm install vim-mode
