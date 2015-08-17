3bee767e0cf873e5bb69b4b6752754dba7bf5793
Joaquim Torres <quim.r.s.torres@gmail.com>
2015-08-18 00:05:44 +0100

    Add more stuff

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -22,23 +22,28 @@ brew tap homebrew/dupes
 brew install apple-gcc42
 
 brew install caskroom/cask/brew-cask
+
+# docker
+brew cask install virtualbox
+brew install boot2docker
+
 brew cask install java
 brew cask install google-chrome
 brew cask install firefox
 brew cask install 1password
 brew cask install google-drive
-brew cask install adium
 brew cask install hipchat
 brew cask install slack
 brew cask install iterm2
  brew cask install alfred
-# brew cask install flashlight
 brew cask install spotify
 brew cask install vlc
 brew cask install utorrent
 brew cask install tvshows
-brew cask install evernote
 brew cask install skype
 brew cask install limechat
 brew cask install gitter
 brew cask install nvalt
+brew cask install kindle
+brew cask install send-to-kindle
+brew cask install telegram
