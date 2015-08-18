e991e8e07afc422696fa769f2b37e4a8d010f0a4
Joaquim Torres <quimrstorres@Joaquims-Air.home>
2015-08-18 01:05:53 +0100

    Update docker and a few other things

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,3 +1,5 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew install zsh
 brew install zsh-completions
 brew install zsh-syntax-highlighting
@@ -6,16 +8,10 @@ brew install tmux
 brew install reattach-to-user-namespace
 brew install git
 brew install ctags
-brew install rbenv
-brew install ruby-build
 brew install ag
 brew install leiningen
-brew install hub
-brew install node
 brew install vim
 brew install --HEAD https://raw.github.com/neovim/neovim/master/neovim.rb
-brew install go
-brew install forego
 brew install autojump
 
 brew tap homebrew/dupes
@@ -25,7 +21,9 @@ brew install caskroom/cask/brew-cask
 
 # docker
 brew cask install virtualbox
-brew install boot2docker
+brew install docker
+brew install docker-machine
+brew install docker-compose
 
 brew cask install java
 brew cask install google-chrome
@@ -33,18 +31,18 @@ brew cask install firefox
 brew cask install 1password
 brew cask install google-drive
 brew cask install dropbox
-brew cask install hipchat
-brew cask install slack
 brew cask install iterm2
- brew cask install alfred
+brew cask install alfred
 brew cask install spotify
 brew cask install vlc
 brew cask install utorrent
 brew cask install tvshows
+brew cask install hipchat
+brew cask install slack
 brew cask install skype
+brew cask install telegram
 brew cask install limechat
 brew cask install gitter
 brew cask install nvalt
 brew cask install kindle
 brew cask install send-to-kindle
-brew cask install telegram
